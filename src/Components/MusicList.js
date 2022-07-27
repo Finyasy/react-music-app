77008690afb1828a7534a41f8979b0951ee49c5c
Bryan Bosire <bryanbosire@gmail.com>
2022-07-27 12:37:23 +0300

    Added FaTimes icon for cursor pointer red

--- src/Components/MusicList.js
+++ src/Components/MusicList.js
@@ -1,9 +1,11 @@
 import React from "react";
+import {FaTimes} from 'react-icons/fa';
 
 const MusicList = ({music}) => {
   return (
-    <div>
-      <h1>{music.songName}</h1>
+    <div className="List">
+      <h1>{music.songName} <FaTimes style={{color:'red', cursor:'pointer'}}  />
+       </h1>
       <h2>{music.artist}</h2>
       <p>{music.lyrics}</p>
       <p>{music.Ratings}</p>
