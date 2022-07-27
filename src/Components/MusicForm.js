5ec22c44bd2b16dcb5114135863226b92c0085ee
Bryan Bosire <bryanbosire@gmail.com>
2022-07-27 20:28:02 +0300

    Added event handleChange and handleSubmit and inputs inside form

--- src/Components/MusicForm.js
+++ src/Components/MusicForm.js
@@ -1,10 +1,58 @@
-import React from "react";
+import React,{useState} from "react";
 
 const MusicForm = () => {
+  const [newMusic, setNewMusic] = useState({
+    songName: "",
+    artist: "",
+    lyrics: "",
+    Ratings: "",   
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setNewMusic({ ...newMusic, [name]: value });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setNewMusic(
+      {
+        songName: "",
+        artist: "",
+        lyrics: "",
+        Ratings: "",
+      }
+    )
+  }
 
   return (
-    <form>
-
+    <form onSubmit={handleSubmit} >
+      <input
+        onChange={handleChange}
+        name="songName"
+        value={newMusic.songName}
+        placeholder="songName"
+      />
+      <input
+        onChange={handleChange}
+        name="artist"
+        value={newMusic.artist}
+        placeholder="artist"
+      />
+      <input
+        onChange={handleChange}
+        name="Ratings"
+        value={newMusic.Ratings}
+        placeholder="Ratings"
+      />
+      <textarea
+        onChange={handleChange}
+        name="lyrics"
+        value={newMusic.lyrics}
+        placeholder="Write your lyrics here..."
+        rows={10}
+      />
+      <input type="submit" value="Share your lyrics" />
     </form>
   );
 };
