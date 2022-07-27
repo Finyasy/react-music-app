4f441c00e83f762798dca0aa68766eeffc411bbb
Bryan Bosire <bryanbosire@gmail.com>
2022-07-27 11:33:03 +0300

    added function onClick for button

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,17 @@
-import React from 'react'
-import Button from './Button'
+import React from "react";
+import Button from "./Button";
 
 function Header({ title }) {
+  const onClick = () => {
+    console.log("Clicked");
+  }
+
   return (
     <header className="header">
-        <h1 style={{color:"red"}}>{title}</h1>
-        <Button />
+      <h1 style={{ color: "red" }}>{title}</h1>
+      <Button onClick={onClick} text ="Add"/>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
