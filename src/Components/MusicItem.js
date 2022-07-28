8bbecdd0dc67dac8456a09b90e862c0f1a343835
Bryan Bosire <bryanbosire@gmail.com>
2022-07-28 12:43:30 +0300

    Passed context instead of props

--- src/Components/MusicItem.js
+++ src/Components/MusicItem.js
@@ -1,15 +1,14 @@
 import React from "react";
 import MusicList from "./MusicList";
 
-function MusicItem ({musics, onDelete}) {
-
+function MusicItem({ musics, onDelete }) {
   return (
-    <div className="music-container" >
+    <div className="music-container">
       {musics.map((music) => (
-        <MusicList  key={music.id} music={music} onDelete={onDelete} />
+        <MusicList key={music.id} music={music} onDelete={onDelete} />
       ))}
     </div>
   );
-};
+}
 
 export default MusicItem;
