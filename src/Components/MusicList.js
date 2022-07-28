3bc97b79c83d7900d686e378aeff96dce5c224fe
Bryan Bosire <bryanbosire@gmail.com>
2022-07-28 12:44:36 +0300

    Added music song by music artist for simplicity

--- src/Components/MusicList.js
+++ src/Components/MusicList.js
@@ -13,7 +13,13 @@ const MusicList = ({ music, onDelete }) => {
       </h1>
       <p>{music.lyrics}</p>
       <h1>{music.Ratings}</h1>
-      <img className="photo" src={music.image} height={250} width={250} alt="" />
+      <img
+        className="photo"
+        src={music.image}
+        height={250}
+        width={250}
+        alt=""
+      />
     </div>
   );
 };
