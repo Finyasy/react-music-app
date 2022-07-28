b994449729f81c2113fa96bec3faf56ff24416bf
Bryan Bosire <bryanbosire@gmail.com>
2022-07-28 12:41:44 +0300

    Added useLocation: This hook returns the location object used by the react-router i.e for button

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import Button from "./Button";
 
-function Header({ title , onAdd }) {
+function Header({ title, onAdd, showAdd }) {
+  const location = useLocation();
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button text="Add" onClick={onAdd} />
+      {location.pathname === '/' && <Button
+        color={showAdd ? "red" : "green"}
+        text={showAdd ? "Close" : "Add"}
+        onClick={onAdd}
+      />}
     </header>
   );
 }
